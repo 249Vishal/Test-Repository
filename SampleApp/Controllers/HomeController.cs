7fe51430010f20ca933e35a6bb3ddc0a3929d167
aziz.nagpurwala <aziz.nagpurwala@NIT-LPT-R028>
2018-04-10 14:20:29 +0530

    Adding mahesh

--- SampleApp/Controllers/HomeController.cs
+++ SampleApp/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace SampleApp.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page. Vishal Ingole , Tanmay Gujar Test, Aziz nagpurwala";
+            ViewBag.Message = "Your contact page. Vishal Ingole , Tanmay Gujar Test, Aziz Nagpurwala , Mahesh Gondugade";
 
             return View();
         }
