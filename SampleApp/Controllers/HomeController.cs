d8a4e2bb4746c5466f79cca68e47a29540e50cbb
tanmay.gujar <tanmay.gujar@NIT-LPT-R027>
2018-04-10 13:06:20 +0530

    Message changed

--- SampleApp/Controllers/HomeController.cs
+++ SampleApp/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace SampleApp.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page. Vishal Ingole , Tanmay Gujar";
+            ViewBag.Message = "Your contact page. Vishal Ingole , Tanmay Gujar Test";
 
             return View();
         }
